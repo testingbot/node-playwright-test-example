bae9ecf5bffc70ba9fb0d135c024d1326421cbae
Max Schmitt <max@schmitt.mx>
2023-06-17 19:29:34 +0200

    chore: connection cleanups

--- testingbot.config.ts
+++ testingbot.config.ts
@@ -1,11 +1,11 @@
-exports.getCdpEndpoint = (userCapabilities) => {
+export function getConnectWsEndpoint(userCapabilities: any) {
     const defaultCapabilities = {
         'tb:options': {
             key: process.env.TB_KEY,
             secret: process.env.TB_SECRET
         }
     }
-    const capabilities = Object.assign(userCapabilities, defaultCapabilities)
-    const cdpUrl = `wss://cloud.testingbot.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
-    return cdpUrl
+    const capabilities = { ...defaultCapabilities, ...userCapabilities }
+    const connectUrl = `wss://cloud.testingbot.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+    return connectUrl
 }
