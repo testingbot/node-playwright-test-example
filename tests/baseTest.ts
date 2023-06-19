e28527a5b964b9cbe84a89058cd6b1283c996097
jochen <jochen@netlog.com>
2023-06-19 09:43:22 +0200

    baseTest

--- tests/baseTest.ts
+++ tests/baseTest.ts
@@ -4,10 +4,12 @@ export const test = base.extend({
   page: async ({ page }, use, testInfo) => {
     await page.evaluate(_ => { }, `testingbot_executor: ${JSON.stringify({ action: 'setSessionName', arguments: { name: testInfo.project.name } })}`)
     await use(page)
-    if (testInfo.status === 'passed')
+    if (testInfo.status === 'passed') {
       await page.evaluate(_ => { }, `testingbot_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { passed: true } })}`)
-    else ["failed", "interrupted", "timedOut"].includes(testInfo.status!)
+    }
+    else if (["failed", "interrupted", "timedOut"].includes(testInfo.status!)) {
       await page.evaluate(_ => { }, `testingbot_executor: ${JSON.stringify({ action: 'setSessionStatus', arguments: { passed: false, reason: testInfo.error?.message } })}`)
+    }
   },
 })
 
